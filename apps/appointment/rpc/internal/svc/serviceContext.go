12caa6c5a9b010c295f25c57bfc71f203ae1dab1
PC1806 <czmudk@163.com>
2026-07-02 17:01:02 +0800

    success to start rpc server

---
+++ apps/appointment/rpc/internal/svc/serviceContext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "github.com/czm-curtis/smart-reserve/apps/appointment/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
